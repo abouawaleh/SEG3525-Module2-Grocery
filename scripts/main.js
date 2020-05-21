9736124ff64dc8f5cf47282672d9a9b67288dcd2
abouawaleh <43045488+abouawaleh@users.noreply.github.com>
2020-05-21 15:53:30 -0400

    Create table for checkbox product list

--- scripts/main.js
+++ scripts/main.js
@@ -36,30 +36,63 @@ function populateListProductChoices(slct1, slct2) {
 		
 	// obtain a reduced list of products based on restrictions
     var optionArray = restrictListProducts(products, s1.value);
-
+    var priceArray = restrictListProductPrices(products, s1.value);
+	//Code pour créer table inspiré de https://stackoverflow.com/questions/14643617/create-table-using-javascript
+	var tbl = document.createElement('table');
+  	tbl.style.width = '100%';
+  	tbl.setAttribute('border', '1');
+	
+  	var thead = document.createElement('thead');
+    	var tr = document.createElement('tr');
+        var th = document.createElement('th');
+        th.appendChild(document.createTextNode('\u0020'));
+        tr.appendChild(th)
+        var th = document.createElement('th');
+        th.appendChild(document.createTextNode('Product'));
+        tr.appendChild(th)
+        var th = document.createElement('th');
+        th.appendChild(document.createTextNode('Price'));
+        tr.appendChild(th)
+      	
+  	var tbdy = document.createElement('tbody');
+	
 	// for each item in the array, create a checkbox element, each containing information such as:
 	// <input type="checkbox" name="product" value="Bread">
 	// <label for="Bread">Bread/label><br>
+  	for (var i = 0; i < optionArray; i++) {
+    		var tr = document.createElement('tr');
 		
-	for (i = 0; i < optionArray.length; i++) {
-			
+        	var td = document.createElement('td');
 		var productName = optionArray[i];
 		// create the checkbox and add in HTML DOM
 		var checkbox = document.createElement("input");
 		checkbox.type = "checkbox";
 		checkbox.name = "product";
 		checkbox.value = productName;
-		s2.appendChild(checkbox);
+		td.appendChild(checkbox);
+        	tr.appendChild(td);
 		
+        	var td = document.createElement('td');
 		// create a label for the checkbox, and also add in HTML DOM
 		var label = document.createElement('label')
 		label.htmlFor = productName;
 		label.appendChild(document.createTextNode(productName));
-		s2.appendChild(label);
+		td.appendChild(label);
+		tr.appendChild(td);
 		
-		// create a breakline node and add in HTML DOM
-		s2.appendChild(document.createElement("br"));    
-	}
+        	var td = document.createElement('td');
+		var productPrice = priceArray[i];
+		// create a label for the checkbox, and also add in HTML DOM
+		var label = document.createElement('label')
+		label.htmlFor = productPrice;
+		label.appendChild(document.createTextNode(productPrice));
+		td.appendChild(label);
+		tr.appendChild(td);
+		
+		tbdy.appendChild(tr);
+      	}    	
+  	tbl.appendChild(tbdy);
+  	s2.appendChild(tbl)
 }
 	
 // This function is called when the "Add selected items to cart" button in clicked
