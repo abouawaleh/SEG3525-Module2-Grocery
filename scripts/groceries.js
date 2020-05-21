cc0afc1289e438a630a9b0910ff437dfb852e528
abouawaleh <43045488+abouawaleh@users.noreply.github.com>
2020-05-21 15:03:13 -0400

    Add restrictListProductPrices function

--- scripts/groceries.js
+++ scripts/groceries.js
@@ -92,18 +92,33 @@ function restrictListProducts(prods, restriction) {
 	let product_names = [];
 	for (let i=0; i<prods.length; i+=1) {
 		if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
-			product_names.push(prods[i].name+"\t\t\t"+prods[i].price);
+			product_names.push(prods[i].name);
 		}
 		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
-			product_names.push(product_names.push(prods[i].name+"\t\t\t"+prods[i].price);
+			product_names.push(product_names.push(prods[i].name);
 		}
 		else if (restriction == "None"){
-			product_names.push(product_names.push(prods[i].name+"\t\t\t"+prods[i].price);
+			product_names.push(product_names.push(prods[i].name);
 		}
 	}
 	return product_names;
 }
-
+					   
+function restrictListProductPrices(prods, restriction) {
+	let product_names = [];
+	for (let i=0; i<prods.length; i+=1) {
+		if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
+			product_names.push(prods[i].price);
+		}
+		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
+			product_names.push(product_names.push(prods[i].price);
+		}
+		else if (restriction == "None"){
+			product_names.push(product_names.push(prods[i].price);
+		}
+	}
+	return product_names;
+}
 // Calculate the total price of items, with received parameter being a list of products
 function getTotalPrice(chosenProducts) {
 	totalPrice = 0;
