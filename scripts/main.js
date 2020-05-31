6da48b7c184e9aa2a02c3044ec4ded2fd3d9aeda
abouawaleh <43045488+abouawaleh@users.noreply.github.com>
2020-05-31 18:09:01 -0400

    Change link for openInfo function

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 
 // This function is called when any of the tab is clicked
-// It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
+// It is adapted from https://www.w3schools.com/howto/howto_js_accordion.asp
 
 function openInfo(panelName) {
 	
